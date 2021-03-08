dfc1a0ed5a801cc8ebe76db2bb11f619150f677b
Data Designer <72868569+Data-Designer@users.noreply.github.com>
2021-03-08 17:32:27 +0800

    Update masking.py

--- utils/masking.py
+++ utils/masking.py
@@ -4,7 +4,7 @@ class TriangularCausalMask():
     def __init__(self, B, L, device="cpu"):
         mask_shape = [B, 1, L, L]
         with torch.no_grad():
-            self._mask = torch.triu(torch.ones(mask_shape, dtype=torch.bool), diagonal=1).to(device)
+            self._mask = torch.triu(torch.ones(mask_shape, dtype=torch.bool), diagonal=1).to(device) # 保留矩阵上三角
 
     @property
     def mask(self):
